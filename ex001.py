22204bbaaa131190cfea10ffdc194579a0e98268
Gutosilqui <silvadeaquino@gmail.com>
2025-03-27 20:14:14 -0300

    primeiro commit de alguns scripts basicos em python

---
+++ ex001.py
@@ -0,0 +1,61 @@
+# DESAFIO 001
+"""
+print('Hello, World')
+msg = 'Hello, World'
+msg1 = 'Hello,'
+msg2 = ' World'
+msgsoma = msg1 + msg2
+print(msg)
+print('{}'.format(msg))
+print(f'{msg1}{msg2}')
+print(msgsoma)
+
+day = input('Dia = ')
+month = input('Mes = ')
+year = input('Ano = ')
+print('Vocé nasceu no dia ' + day + '/' + month + '/' + year + '. Correto?')
+print('Vocé nasceu no dia ',day,'/',month,'/',year,'. Correto?')
+"""
+
+# DESAFIO 002
+"""
+name = input('Qual é o seu nome? ')
+print('Olá ' + name + '! Prazer em te conhecer!')
+print('Olá {}! Prazer em te conhecer!'.format(name))
+print('Olá', name,'! Prazer em te conhecer!')
+"""
+
+# DESAFIO 003
+"""
+number1 = int(input('Primeiro número '))
+number2 = int(input('Segundo número '))
+soma = number1 + number2
+print('A soma é ', soma)
+
+n1 = input('Primeiro número ')
+n2 = input('Segundo número ')
+soma = int(n1) + int(n2)
+print ('A soma é', soma)
+"""
+
+# DESAFIO 004
+"""
+valor = input('Digite qualquer coisa seja números ou palavras = ')
+print(f'O tipo primitivo desse valor é {type(valor)}')
+
+print(f'É númerico? {valor.isnumeric()}')
+print(f'É decimal? {valor.isdecimal()}')
+print(f'É dígito? {valor.isdigit()}')
+print(f'É alfabético? {valor.isalpha()}')
+print(f'É alfanumérico? {valor.isalnum()}')
+print(f'Está em maiúsculas? {valor.isupper()}')
+print(f'Está em minúsculas? {valor.islower()}')
+print(f'Contém apenas espaços? {valor.isspace()}')
+print(f'Começa com letra maiúscula? {valor.istitle()}')
+"""
+
+# DESAFIO 005
+"""
+
+"""
+
